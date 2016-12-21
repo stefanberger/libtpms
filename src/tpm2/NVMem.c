b19b6ed3435adcd2aab8fcf5ecd4def8c61681c2
Stefan Berger <stefanb@linux.vnet.ibm.com>
2016-12-21 16:26:19 -0500

    Fix compilation errors in the TPM 2 code
    Fix compilation errors in the TPM 2 code.
    
    Signed-off-by: Stefan Berger <stefanb@linux.vnet.ibm.com>

--- src/tpm2/NVMem.c
+++ src/tpm2/NVMem.c
@@ -142,12 +142,14 @@ _plat__NVEnable(
 	}
     else
 	{
+	    size_t ignore;
 	    // If NVChip file exist, assume the size is correct
 	    fseek(s_NVFile, 0, SEEK_END);
 	    assert(ftell(s_NVFile) == NV_MEMORY_SIZE);
 	    // read NV file data to memory
 	    fseek(s_NVFile, 0, SEEK_SET);
-	    fread(s_NV, NV_MEMORY_SIZE, 1, s_NVFile);
+	    ignore = fread(s_NV, NV_MEMORY_SIZE, 1, s_NVFile);
+	    (void)ignore;
 	}
 #endif
     // NV contents have been read and the error checks have been performed. For
