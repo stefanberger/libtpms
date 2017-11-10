81a0665cea3cf09fc06f1d8be154c6934d524372
Stefan Berger <stefanb@linux.vnet.ibm.com>
2017-11-10 17:21:42 -0500

    tpm2: Choose the TPM2 NVRAM memory to be 64kb
    For this to work increase the maximum allocatable memory chunk
    supported by TPM_Malloc() to 128k.
    
    Signed-off-by: Stefan Berger <stefanb@linux.vnet.ibm.com>

--- src/tpm2/Implementation.h
+++ src/tpm2/Implementation.h
@@ -355,7 +355,7 @@
 #define  MAX_NV_INDEX_SIZE              2048
 #define  MAX_NV_BUFFER_SIZE             1024
 #define  MAX_CAP_BUFFER                 1024
-#define  NV_MEMORY_SIZE                 16384
+#define  NV_MEMORY_SIZE                 (64 * 1024)
 #define  MIN_COUNTER_INDICES            8
 #define  NUM_STATIC_PCR                 16
 #define  MAX_ALG_LIST_SIZE              64
