37ad1358ebb760356ffb4a4703493edb20d8d879
Stefan Berger <stefanb@linux.vnet.ibm.com>
2018-08-27 13:24:57 -0400

    rev147: Change parameter to TPMI_ALG_HASH_Unmarshal to NO
    Signed-off-by: Stefan Berger <stefanb@linux.vnet.ibm.com>

--- src/tpm2/Unmarshal.c
+++ src/tpm2/Unmarshal.c
@@ -2805,7 +2805,7 @@ TPMS_SCHEME_XOR_Unmarshal(TPMS_SCHEME_XOR *target, BYTE **buffer, INT32 *size)
     TPM_RC rc = TPM_RC_SUCCESS;
     
     if (rc == TPM_RC_SUCCESS) {
-	rc = TPMI_ALG_HASH_Unmarshal(&target->hashAlg, buffer, size, YES);
+	rc = TPMI_ALG_HASH_Unmarshal(&target->hashAlg, buffer, size, NO);	/* as of rev 147 */
     }
     if (rc == TPM_RC_SUCCESS) {
 	rc = TPMI_ALG_KDF_Unmarshal(&target->kdf, buffer, size, YES);
