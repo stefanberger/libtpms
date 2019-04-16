d595e62b9f71ceed383f2d26b875d1578cfe8474
Marc-André Lureau <marcandre.lureau@redhat.com>
2019-04-16 12:56:52 +0200

    build-sys: don't tweak LD_LIBRARY_PATH
    libtool already provides helper scripts around executables.
    
    Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>

--- tests/run-fuzzer.sh
+++ tests/run-fuzzer.sh
@@ -5,17 +5,6 @@
 DIR=${PWD}/$(dirname "$0")
 ROOT=${DIR}/..
 WORKDIR=$(mktemp -d)
-export LD_LIBRARY_PATH=${ROOT}/src/.libs
-
-if ! [ -d ${LD_LIBRARY_PATH} ]; then
-	echo "Wrong path to libtpms library: ${LD_LIBRARY_PATH}"
-	exit 1
-fi
-
-if ! [ -f "$(readlink -f ${LD_LIBRARY_PATH}/libtpms.so)" ]; then
-	echo "Cannot find libtpms at ${LD_LIBRARY_PATH}/libtpms.so"
-	exit 1
-fi
 
 function cleanup()
 {
