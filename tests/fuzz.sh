5b3326ca71957b595666c77cb3d097ca6820b52e
Stefan Berger <stefanb@linux.vnet.ibm.com>
2019-05-04 09:28:11 -0400

    tests: Restrict number of command line parameters to fuzz
    Restrict the number of command line parameters passed to the
    fuzz program in order not to overstep command line parameter
    size restrictions.
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- tests/fuzz.sh
+++ tests/fuzz.sh
@@ -3,4 +3,17 @@
 TESTDIR=${abs_top_testdir:-$(dirname "$0")}
 DIR=${PWD}
 
-${DIR}/fuzz "$TESTDIR/corpus-execute-command/"*
+MAXLINES=128
+l=1
+
+corpus=$(ls "$TESTDIR/corpus-execute-command/"*)
+
+while :; do
+  echo "Passing test cases $l to $((l + MAXLINES))"
+  tmp=$(echo "${corpus}" | sed -n "${l},$((l + MAXLINES))p")
+  [ -z "${tmp}" ] && exit 0
+  ${DIR}/fuzz ${tmp}
+  rc=$?
+  [ $rc -ne 0 ] && exit $rc
+  l=$((l + MAXLINES))
+done
