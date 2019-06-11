dad52fb3a887271a0d89dcb1dc1ba607baf38134
Stefan Berger <stefanb@linux.vnet.ibm.com>
2019-06-11 15:08:16 -0400

    tpm2: rev155: Implement TpmLogFailure and modify for libtpms
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- src/tpm2/TpmFail_fp.h
+++ src/tpm2/TpmFail_fp.h
@@ -1,9 +1,9 @@
 /********************************************************************************/
 /*										*/
-/*			     				*/
+/*			     Failure Mode Handling				*/
 /*			     Written by Ken Goldman				*/
 /*		       IBM Thomas J. Watson Research Center			*/
-/*            $Id: TpmFail_fp.h 953 2017-03-06 20:31:40Z kgoldman $			*/
+/*            $Id: TpmFail_fp.h 1476 2019-06-10 19:32:03Z kgoldman $		*/
 /*										*/
 /*  Licenses and Notices							*/
 /*										*/
@@ -55,7 +55,7 @@
 /*    arising in any way out of use or reliance upon this specification or any 	*/
 /*    information herein.							*/
 /*										*/
-/*  (c) Copyright IBM Corp. and others, 2016					*/
+/*  (c) Copyright IBM Corp. and others, 2016 - 2019				*/
 /*										*/
 /********************************************************************************/
 
@@ -70,6 +70,14 @@ SetForceFailureMode(
 		    void
 		    );
 #endif /* libtpms added */
+void
+TpmLogFailure(
+#if FAIL_TRACE
+	      const char      *function,
+	      int              line,
+#endif
+	      int              code
+	      );
 NORETURN void
 TpmFail(
 #if FAIL_TRACE   /* libtpms added begin */
