04b3262da299b6f03f2fa748b005ed1eca1b929c
Stefan Berger <stefanb@linux.vnet.ibm.com>
2019-06-11 10:36:06 -0400

    tpm2: rev155: Have DAStartup return BOOL
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- src/tpm2/DA_fp.h
+++ src/tpm2/DA_fp.h
@@ -3,7 +3,7 @@
 /*			     				*/
 /*			     Written by Ken Goldman				*/
 /*		       IBM Thomas J. Watson Research Center			*/
-/*            $Id: DA_fp.h 809 2016-11-16 18:31:54Z kgoldman $			*/
+/*            $Id: DA_fp.h 1476 2019-06-10 19:32:03Z kgoldman $			*/
 /*										*/
 /*  Licenses and Notices							*/
 /*										*/
@@ -55,7 +55,7 @@
 /*    arising in any way out of use or reliance upon this specification or any 	*/
 /*    information herein.							*/
 /*										*/
-/*  (c) Copyright IBM Corp. and others, 2016					*/
+/*  (c) Copyright IBM Corp. and others, 2016 - 2019				*/
 /*										*/
 /********************************************************************************/
 
@@ -70,7 +70,7 @@ void
 DAInit(
        void
        );
-void
+BOOL
 DAStartup(
 	  STARTUP_TYPE     type           // IN: startup type
 	  );
