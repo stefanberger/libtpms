c58d60398b098a2ea7d737d39a5b489de4b99eaf
Stefan Berger <stefanb@linux.vnet.ibm.com>
2019-06-11 10:32:12 -0400

    tpm2: rev155: Have TPMRegisterStartup return BOOL
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- src/tpm2/Power_fp.h
+++ src/tpm2/Power_fp.h
@@ -1,9 +1,9 @@
 /********************************************************************************/
 /*										*/
-/*			     				*/
+/*	Functions That Receive the Simulated Power State Transitions of the TPM	*/
 /*			     Written by Ken Goldman				*/
 /*		       IBM Thomas J. Watson Research Center			*/
-/*            $Id: Power_fp.h 809 2016-11-16 18:31:54Z kgoldman $			*/
+/*            $Id: Power_fp.h 1476 2019-06-10 19:32:03Z kgoldman $		*/
 /*										*/
 /*  Licenses and Notices							*/
 /*										*/
@@ -55,7 +55,7 @@
 /*    arising in any way out of use or reliance upon this specification or any 	*/
 /*    information herein.							*/
 /*										*/
-/*  (c) Copyright IBM Corp. and others, 2016					*/
+/*  (c) Copyright IBM Corp. and others, 2016 - 2019				*/
 /*										*/
 /********************************************************************************/
 
@@ -66,7 +66,7 @@ void
 TPMInit(
 	void
 	);
-void
+BOOL
 TPMRegisterStartup(
 		   void
 		   );
