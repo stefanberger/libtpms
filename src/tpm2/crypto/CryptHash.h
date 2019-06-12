296cbb1699685fdbfa9e38096b055ca45f0ec3b3
Stefan Berger <stefanb@linux.vnet.ibm.com>
2019-06-12 10:05:34 -0400

    tpm2: rev155: Refactor hash templates; for now also keep old templates
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- src/tpm2/crypto/CryptHash.h
+++ src/tpm2/crypto/CryptHash.h
@@ -223,19 +223,21 @@ typedef const struct
 /* Macro to fill in the HASH_DEF for an algorithm. For SHA1, the instance would be:
    HASH_DEF_TEMPLATE(Sha1, SHA1) This handles the difference in capitalization for the various
    pieces. */
-#define HASH_DEF_TEMPLATE(HASH)						\
-    HASH_DEF    HASH##_Def= {						\
-	{(HASH_START_METHOD *)&tpmHashStart_##HASH,			\
-	 (HASH_DATA_METHOD *)&tpmHashData_##HASH,			\
-	 (HASH_END_METHOD *)&tpmHashEnd_##HASH,				\
-	 (HASH_STATE_COPY_METHOD *)&tpmHashStateCopy_##HASH,		\
-	 (HASH_STATE_EXPORT_METHOD *)&tpmHashStateExport_##HASH,	\
-	 (HASH_STATE_IMPORT_METHOD *)&tpmHashStateImport_##HASH,	\
-	},								\
-	HASH##_BLOCK_SIZE,     /*block size */				\
-	HASH##_DIGEST_SIZE,    /*data size */				\
-	sizeof(tpmHashState##HASH##_t),					\
-	TPM_ALG_##HASH}
+
+#define HASH_DEF_TEMPLATE(HASH, Hash)					\
+    HASH_DEF    Hash##_Def= {						\
+			     {(HASH_START_METHOD *)&tpmHashStart_##HASH, \
+			      (HASH_DATA_METHOD *)&tpmHashData_##HASH,	\
+			      (HASH_END_METHOD *)&tpmHashEnd_##HASH,	\
+			      (HASH_STATE_COPY_METHOD *)&tpmHashStateCopy_##HASH, \
+			      (HASH_STATE_EXPORT_METHOD *)&tpmHashStateExport_##HASH, \
+			      (HASH_STATE_IMPORT_METHOD *)&tpmHashStateImport_##HASH, \
+			     },						\
+			     HASH##_BLOCK_SIZE,     /*block size */	\
+			     HASH##_DIGEST_SIZE,    /*data size */	\
+			     sizeof(tpmHashState##HASH##_t),		\
+			     TPM_ALG_##HASH};
+
 /* These definitions are for the types that can be in a hash state structure. These types are used
    in the cryptographic utilities. This is a define rather than an enum so that the size of this
    field can be explicit. */
