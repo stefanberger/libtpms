a1e3bee3c155a15039300625f55c7ee4a25f04d9
Stefan Berger <stefanb@linux.vnet.ibm.com>
2019-06-12 17:06:47 -0400

    tpm2: rev155: Use MakeDerTag instead of CryptHashGetDer
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- src/tpm2/crypto/openssl/CryptRsa.c
+++ src/tpm2/crypto/openssl/CryptRsa.c
@@ -3,7 +3,7 @@
 /*		Implementation of cryptographic primitives for RSA		*/
 /*			     Written by Ken Goldman				*/
 /*		       IBM Thomas J. Watson Research Center			*/
-/*            $Id: CryptRsa.c 1409 2019-01-14 20:39:15Z kgoldman $		*/
+/*            $Id: CryptRsa.c 1476 2019-06-10 19:32:03Z kgoldman $		*/
 /*										*/
 /*  Licenses and Notices							*/
 /*										*/
@@ -721,23 +721,28 @@ RSASSA_Encode(
 	      TPM2B               *hIn        // IN: digest value to encode
 	      )
 {
-    const BYTE      *der;
+    BYTE             DER[20];
+    BYTE            *der = DER;
+    INT32            derSize = MakeDerTag(hashAlg, sizeof(DER), DER);
     BYTE            *eOut;
-    INT32            derSize = CryptHashGetDer(hashAlg, &der);
     INT32            fillSize;
     TPM_RC           retVal = TPM_RC_SUCCESS;
+    
     // Can't use this scheme if the algorithm doesn't have a DER string defined.
     if(derSize == 0)
 	ERROR_RETURN(TPM_RC_SCHEME);
+    
     // If the digest size of 'hashAl' doesn't match the input digest size, then
     // the DER will misidentify the digest so return an error
     if(CryptHashGetDigestSize(hashAlg) != hIn->size)
 	ERROR_RETURN(TPM_RC_VALUE);
     fillSize = pOut->size - derSize - hIn->size - 3;
     eOut = pOut->buffer;
+    
     // Make sure that this combination will fit in the provided space
     if(fillSize < 8)
 	ERROR_RETURN(TPM_RC_SIZE);
+    
     // Start filling
     *eOut++ = 0; // initial byte of zero
     *eOut++ = 1; // byte of 0x01
@@ -766,25 +771,30 @@ RSASSA_Decode(
 	      )
 {
     BYTE             fail;
-    const BYTE      *der;
+    BYTE             DER[20];
+    BYTE            *der = DER;
+    INT32            derSize = MakeDerTag(hashAlg, sizeof(DER), DER);
     BYTE            *pe;
-    INT32            derSize = CryptHashGetDer(hashAlg, &der);
     INT32            hashSize = CryptHashGetDigestSize(hashAlg);
     INT32            fillSize;
     TPM_RC           retVal;
     BYTE            *digest;
     UINT16           digestSize;
+    
     pAssert(hIn != NULL && eIn != NULL);
     pe = eIn->buffer;
+    
     // Can't use this scheme if the algorithm doesn't have a DER string
     // defined or if the provided hash isn't the right size
     if(derSize == 0 || (unsigned)hashSize != hIn->size)
 	ERROR_RETURN(TPM_RC_SCHEME);
+    
     // Make sure that this combination will fit in the provided space
     // Since no data movement takes place, can just walk though this
     // and accept nearly random values. This can only be called from
     // CryptValidateSignature() so eInSize is known to be in range.
     fillSize = eIn->size - derSize - hashSize - 3;
+    
     // Start checking (fail will become non-zero if any of the bytes do not have
     // the expected value.
     fail = *pe++;                   // initial byte of zero
