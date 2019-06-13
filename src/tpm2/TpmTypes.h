52b7c83927c31f3d7fcdf3cafb63d7b273a43d8b
Stefan Berger <stefanb@linux.vnet.ibm.com>
2019-06-13 08:49:13 -0400

    tpm2: rev155: Set spec version to 155
    With some x509 related issues fixed, advertise revision 155 now
    so that IBM TSS 2 test suite passes the X509 tests.
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- src/tpm2/TpmTypes.h
+++ src/tpm2/TpmTypes.h
@@ -315,11 +315,11 @@ typedef  UINT32             TPM_SPEC;
 #define TPM_SPEC_FAMILY         (TPM_SPEC)(SPEC_FAMILY)
 #define SPEC_LEVEL              00
 #define TPM_SPEC_LEVEL          (TPM_SPEC)(SPEC_LEVEL)
-#define SPEC_VERSION            150
+#define SPEC_VERSION            155
 #define TPM_SPEC_VERSION        (TPM_SPEC)(SPEC_VERSION)
-#define SPEC_YEAR               2018
+#define SPEC_YEAR               2019
 #define TPM_SPEC_YEAR           (TPM_SPEC)(SPEC_YEAR)
-#define SPEC_DAY_OF_YEAR        262
+#define SPEC_DAY_OF_YEAR        107
 #define TPM_SPEC_DAY_OF_YEAR    (TPM_SPEC)(SPEC_DAY_OF_YEAR)
 
 /* Table 2:7 - Definition of TPM_GENERATED Constants */
