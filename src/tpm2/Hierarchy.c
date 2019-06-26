4ef40892ac84cc2e74ec370451b246a52865d2d1
Stefan Berger <stefanb@linux.vnet.ibm.com>
2019-06-26 11:54:47 -0400

    tpm2: Add COMPAT_LEVEL to nullSeed to track compatibility level
    Add COMPAT_LEVEL to the nullSeed in the state_reset data to track
    its compatibility level.
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- src/tpm2/Hierarchy.c
+++ src/tpm2/Hierarchy.c
@@ -154,6 +154,7 @@ HierarchyStartup(
 	    CryptRandomGenerate(gr.nullProof.t.size, gr.nullProof.t.buffer);
 	    gr.nullSeed.t.size = sizeof(gr.nullSeed.t.buffer);
 	    CryptRandomGenerate(gr.nullSeed.t.size, gr.nullSeed.t.buffer);
+	    gr.nullSeedCompatLevel = COMPAT_LEVEL_LAST;
 	}
     return;
 }
@@ -235,7 +236,7 @@ HierarchyGetPrimarySeedCompatLevel(
 	    return gp.EPSeedCompatLevel;
 	    break;
 	  case TPM_RH_NULL:
-	    return COMPAT_LEVEL_LAST;
+	    return gr.nullSeedCompatLevel;
 	  default:
 	    FAIL(FATAL_ERROR_INTERNAL);
 	    break;
