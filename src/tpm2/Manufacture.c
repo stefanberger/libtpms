a93450e2242b87003f1e42171db761f1fe67971c
Stefan Berger <stefanb@linux.vnet.ibm.com>
2019-11-17 14:04:07 -0500

    tpm2: Sync unused TpmSizeChecks function
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- src/tpm2/Manufacture.c
+++ src/tpm2/Manufacture.c
@@ -3,7 +3,7 @@
 /*			Performs the manufacturing of the TPM 			*/
 /*			     Written by Ken Goldman				*/
 /*		       IBM Thomas J. Watson Research Center			*/
-/*            $Id: Manufacture.c 1490 2019-07-26 21:13:22Z kgoldman $		*/
+/*            $Id: Manufacture.c 1519 2019-11-15 20:43:51Z kgoldman $		*/
 /*										*/
 /*  Licenses and Notices							*/
 /*										*/
@@ -55,7 +55,7 @@
 /*    arising in any way out of use or reliance upon this specification or any 	*/
 /*    information herein.							*/
 /*										*/
-/*  (c) Copyright IBM Corp. and others, 2016 - 2018				*/
+/*  (c) Copyright IBM Corp. and others, 2016 - 2019				*/
 /*										*/
 /********************************************************************************/
 
@@ -89,7 +89,8 @@ TPM_Manufacture(
 #if RUNTIME_SIZE_CHECKS
     // Call the function to verify the sizes of values that result from different
     // compile options.
-    TpmSizeChecks();
+    if(!TpmSizeChecks())
+	return -1;
 #endif
     // If TPM has been manufactured, return indication.
     if(!firstTime && g_manufactured)
