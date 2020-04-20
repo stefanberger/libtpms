4fc81d6fdacab5eaf3f5c6dff189ec11790c874b
Stefan Berger <stefanb@linux.vnet.ibm.com>
2020-04-20 08:23:22 -0400

    tpm2: rev162: Add #define SYMMETRIC_ALIGNMENT
    Add the #define SYMMETRIC_ALIGNMENT that aligns the tpmCryptKeySchedule_t
    size. Since this tpmCryptKeySchedule_t only seems to be used as a stack
    variable and the alignment field is never accessed nor the size of the
    structre taken, it shouldn't affect anything.
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- src/tpm2/crypto/openssl/TpmToOsslMath.h
+++ src/tpm2/crypto/openssl/TpmToOsslMath.h
@@ -3,7 +3,7 @@
 /*	 		TPM to OpenSSL BigNum Shim Layer			*/
 /*			     Written by Ken Goldman				*/
 /*		       IBM Thomas J. Watson Research Center			*/
-/*            $Id: TpmToOsslMath.h 1519 2019-11-15 20:43:51Z kgoldman $		*/
+/*            $Id: TpmToOsslMath.h 1594 2020-03-26 22:15:48Z kgoldman $		*/
 /*										*/
 /*  Licenses and Notices							*/
 /*										*/
@@ -55,7 +55,7 @@
 /*    arising in any way out of use or reliance upon this specification or any 	*/
 /*    information herein.							*/
 /*										*/
-/*  (c) Copyright IBM Corp. and others, 2016 - 2019				*/
+/*  (c) Copyright IBM Corp. and others, 2016 - 2020				*/
 /*										*/
 /********************************************************************************/
 
@@ -70,6 +70,9 @@
 #define MATH_LIB_OSSL
 #include <openssl/evp.h>
 #include <openssl/ec.h>
+
+#define SYMMETRIC_ALIGNMENT RADIX_BYTES
+
 #if 0 // libtpms added
 #if OPENSSL_VERSION_NUMBER >= 0x10200000L
 // Check the bignum_st definition in crypto/bn/bn_lcl.h and either update the
