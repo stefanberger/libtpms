0e3d5d05f0b86091d714100b854e07f16d90e86b
Stefan Berger <stefanb@linux.ibm.com>
2021-02-17 09:53:54 -0500

    tpm2: Add comment to buggy RSAAdjustPrimeCandidate
    This patch adds a description to the buggy RSAAdjustPrimeCandidate
    implementation that can only be properly fixed after 0.7.x.
    
    Reported-by: Nicolas Iooss <nicolas.iooss@ledger.fr>
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- src/tpm2/crypto/openssl/CryptPrime.c
+++ src/tpm2/crypto/openssl/CryptPrime.c
@@ -308,6 +308,16 @@ RsaAdjustPrimeCandidate(
 {
     UINT16  highBytes;
     crypt_uword_t       *msw = &prime->d[prime->size - 1];
+
+    /* This computation is known to be buggy on 64-bit systems, as MASK=0xffff
+     * instead of 0x0000ffffffffffff and this introduces 32 zero bits in the
+     * adjusted prime number. But fixing this would modify keys which were
+     * previously generated, so keep it this way for now.
+     * This was fixed in the current version of "Trusted Platform Module Library
+     * Family 2.0 Specification - Part 4: Routines - Code" (from TCG) and this issue
+     * will be fixed for newly-generated prime numbers for the version of libtpms
+     * which will come after 0.7.x.
+     */
 #define MASK (MAX_CRYPT_UWORD >> (RADIX_BITS - 16))
     highBytes = *msw >> (RADIX_BITS - 16);
     // This is fixed point arithmetic on 16-bit values
