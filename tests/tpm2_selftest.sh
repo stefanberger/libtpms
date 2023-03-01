32f038944235917ee122ece6706c54d3a0ab19ea
Stefan Berger <stefanb@linux.ibm.com>
2023-03-01 12:50:27 -0500

    tests: Refactor repetitive test scripts to use one common script
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- tests/tpm2_selftest.sh
+++ tests/tpm2_selftest.sh
@@ -2,36 +2,7 @@
 
 # For the license, see the LICENSE file in the root directory.
 
-ROOT=${abs_top_builddir:-$(pwd)/..}
-TESTDIR=${abs_top_testdir:-$(dirname "$0")}
-DIR=${PWD}
+DIR=$(dirname "$0")
 
-WORKDIR=$(mktemp -d)
-
-. ${TESTDIR}/common
-
-function cleanup()
-{
-	rm -rf ${WORKDIR}
-}
-
-trap "cleanup" QUIT EXIT
-
-pushd $WORKDIR &>/dev/null
-
-${DIR}/tpm2_selftest
-rc=$?
-
-fs=$(get_filesize NVChip)
-[ $? -ne 0 ] && exit 1
-exp=176832
-if [ $fs -ne $exp ]; then
-	echo "Error: Unexpected size of NVChip file."
-	echo "Expected: $exp"
-	echo "Got     : $fs"
-	rc=1
-fi
-
-popd &>/dev/null
-
-exit $rc
+"${DIR}/tpm2_run_test.sh" tpm2_selftest
+exit $?
