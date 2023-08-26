c579a3c8de519ee9691e0c0b1df0fb139f8c995e
Stefan Berger <stefanb@linux.ibm.com>
2023-08-25 23:04:50 -0400

    tpm2: Sync RSAEP with upstream
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- src/tpm2/crypto/openssl/CryptRsa.c
+++ src/tpm2/crypto/openssl/CryptRsa.c
@@ -231,16 +231,20 @@ RSAEP(
       )
 {
     TPM2B_TYPE(4BYTES, 4);
-    TPM2B_4BYTES(e) = {{4, {(BYTE)((RSA_DEFAULT_PUBLIC_EXPONENT >> 24) & 0xff),
-			    (BYTE)((RSA_DEFAULT_PUBLIC_EXPONENT >> 16) & 0xff),
-			    (BYTE)((RSA_DEFAULT_PUBLIC_EXPONENT >> 8) & 0xff),
-			    (BYTE)((RSA_DEFAULT_PUBLIC_EXPONENT)& 0xff)}}};
+    TPM2B_4BYTES        e2B;
+    UINT32              e = key->publicArea.parameters.rsaDetail.exponent;
     //
-    if(key->publicArea.parameters.rsaDetail.exponent != 0)
-	UINT32_TO_BYTE_ARRAY(key->publicArea.parameters.rsaDetail.exponent,
-			     e.t.buffer);
-    return ModExpB(dInOut->size, dInOut->buffer, dInOut->size, dInOut->buffer,
-		   e.t.size, e.t.buffer, key->publicArea.unique.rsa.t.size,
+    if(e == 0)
+	e = RSA_DEFAULT_PUBLIC_EXPONENT;
+    UINT32_TO_BYTE_ARRAY(e, e2B.t.buffer);
+    e2B.t.size = 4;
+    return ModExpB(dInOut->size,
+		   dInOut->buffer,
+		   dInOut->size,
+		   dInOut->buffer,
+		   e2B.t.size,
+		   e2B.t.buffer,
+		   key->publicArea.unique.rsa.t.size,
 		   key->publicArea.unique.rsa.t.buffer);
 }
 /* 10.2.17.4.4 RSADP() */
