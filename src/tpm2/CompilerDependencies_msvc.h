fd2e55e3a58f9ca85ccffc7864d70e1514151cc9
Stefan Berger <stefanb@linux.ibm.com>
2023-12-11 14:22:03 -0500

    rev180: Add compiler dependencies header for gcc and msvc
    Add compiler dependencies files for gcc and msvc and remove some of
    the existing #define from CompilerDependencies.h
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- src/tpm2/CompilerDependencies_msvc.h
+++ src/tpm2/CompilerDependencies_msvc.h
@@ -58,49 +58,68 @@
 /*										*/
 /********************************************************************************/
 
-// This file contains the build switches. This contains switches for multiple
-// versions of the crypto-library so some may not apply to your environment.
+// This file contains compiler specific switches.
+// These definitions are for the Microsoft compiler
 //
 
-#ifndef _COMPILER_DEPENDENCIES_H_
-#define _COMPILER_DEPENDENCIES_H_
+#ifndef _COMPILER_DEPENDENCIES_MSVC_H_
+#define _COMPILER_DEPENDENCIES_MSVC_H_
 
-#if defined(__GNUC__)
-#  include "CompilerDependencies_gcc.h"
-#elif defined(_MSC_VER)
-#  include "CompilerDependencies_msvc.h"
+#if !defined(_MSC_VER)
+#  error CompilerDependencies_msvc.h included for wrong compiler
+#endif
+
+// Endian conversion for aligned structures
+#define REVERSE_ENDIAN_16(_Number) _byteswap_ushort(_Number)
+#define REVERSE_ENDIAN_32(_Number) _byteswap_ulong(_Number)
+#define REVERSE_ENDIAN_64(_Number) _byteswap_uint64(_Number)
+
+// Avoid compiler warning for in line of stdio (or not)
+//#define _NO_CRT_STDIO_INLINE
+
+// This macro is used to handle LIB_EXPORT of function and variable names in lieu
+// of a .def file. Visual Studio requires that functions be explicitly exported and
+// imported.
+#ifdef TPM_AS_DLL
+#  define LIB_EXPORT __declspec(dllexport)  // VS compatible version
+#  define LIB_IMPORT __declspec(dllimport)
 #else
-#  error unexpected
-#endif
-
-#include <stdint.h>
-
-// Things that are not defined should be defined as NULL
-
-#ifndef NORETURN
-#  define NORETURN
-#endif
-#ifndef LIB_EXPORT
+// building static libraries
 #  define LIB_EXPORT
-#endif
-#ifndef LIB_IMPORT
 #  define LIB_IMPORT
 #endif
-#ifndef _REDUCE_WARNING_LEVEL_
-#  define _REDUCE_WARNING_LEVEL_(n)
-#endif
-#ifndef _NORMAL_WARNING_LEVEL_
-#  define _NORMAL_WARNING_LEVEL_
-#endif
-#ifndef NOT_REFERENCED
-#  define NOT_REFERENCED(x) (x = x)
+
+#define TPM_INLINE inline
+
+// This is defined to indicate a function that does not return. Microsoft compilers
+// do not support the _Noretrun function parameter.
+#define NORETURN __declspec(noreturn)
+#if _MSC_VER >= 1400  // SAL processing when needed
+#  include <sal.h>
 #endif
 
-#ifdef _POSIX_
-typedef int SOCKET;
-#endif
-// #ifdef TPM_POSIX
-// typedef int SOCKET;
-// #endif
+// #  ifdef _WIN64
+// #    define _INTPTR 2
+// #  else
+// #    define _INTPTR 1
+// #  endif
 
-#endif  // _COMPILER_DEPENDENCIES_H_
+#define NOT_REFERENCED(x) (x)
+
+// Lower the compiler error warning for system include
+// files. They tend not to be that clean and there is no
+// reason to sort through all the spurious errors that they
+// generate when the normal error level is set to /Wall
+#define _REDUCE_WARNING_LEVEL_(n) __pragma(warning(push, n))
+// Restore the compiler warning level
+#define _NORMAL_WARNING_LEVEL_ __pragma(warning(pop))
+#include <stdint.h>
+
+#ifdef TPM_STATIC_ASSERT
+#  error TPM_STATIC_ASSERT already defined
+#endif
+
+// MSVC: failure results in error C2118: negative subscript error
+#define TPM_STATIC_ASSERT(e) typedef char __C_ASSERT__[(e) ? 1 : -1]
+
+#endif  // _COMPILER_DEPENDENCIES_MSVC_H_
