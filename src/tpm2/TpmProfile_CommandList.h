cd6ef13bb11a1368a8b56b070d4207225a8dd1f5
Stefan Berger <stefanb@linux.ibm.com>
2023-12-11 18:04:10 -0500

    rev180: Move #defines from TpmProfile.h into other header files
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

---
+++ src/tpm2/TpmProfile_CommandList.h
@@ -0,0 +1,231 @@
+/********************************************************************************/
+/*										*/
+/*						*/
+/*			     Written by Ken Goldman				*/
+/*		       IBM Thomas J. Watson Research Center			*/
+/*										*/
+/*  Licenses and Notices							*/
+/*										*/
+/*  1. Copyright Licenses:							*/
+/*										*/
+/*  - Trusted Computing Group (TCG) grants to the user of the source code in	*/
+/*    this specification (the "Source Code") a worldwide, irrevocable, 		*/
+/*    nonexclusive, royalty free, copyright license to reproduce, create 	*/
+/*    derivative works, distribute, display and perform the Source Code and	*/
+/*    derivative works thereof, and to grant others the rights granted herein.	*/
+/*										*/
+/*  - The TCG grants to the user of the other parts of the specification 	*/
+/*    (other than the Source Code) the rights to reproduce, distribute, 	*/
+/*    display, and perform the specification solely for the purpose of 		*/
+/*    developing products based on such documents.				*/
+/*										*/
+/*  2. Source Code Distribution Conditions:					*/
+/*										*/
+/*  - Redistributions of Source Code must retain the above copyright licenses, 	*/
+/*    this list of conditions and the following disclaimers.			*/
+/*										*/
+/*  - Redistributions in binary form must reproduce the above copyright 	*/
+/*    licenses, this list of conditions	and the following disclaimers in the 	*/
+/*    documentation and/or other materials provided with the distribution.	*/
+/*										*/
+/*  3. Disclaimers:								*/
+/*										*/
+/*  - THE COPYRIGHT LICENSES SET FORTH ABOVE DO NOT REPRESENT ANY FORM OF	*/
+/*  LICENSE OR WAIVER, EXPRESS OR IMPLIED, BY ESTOPPEL OR OTHERWISE, WITH	*/
+/*  RESPECT TO PATENT RIGHTS HELD BY TCG MEMBERS (OR OTHER THIRD PARTIES)	*/
+/*  THAT MAY BE NECESSARY TO IMPLEMENT THIS SPECIFICATION OR OTHERWISE.		*/
+/*  Contact TCG Administration (admin@trustedcomputinggroup.org) for 		*/
+/*  information on specification licensing rights available through TCG 	*/
+/*  membership agreements.							*/
+/*										*/
+/*  - THIS SPECIFICATION IS PROVIDED "AS IS" WITH NO EXPRESS OR IMPLIED 	*/
+/*    WARRANTIES WHATSOEVER, INCLUDING ANY WARRANTY OF MERCHANTABILITY OR 	*/
+/*    FITNESS FOR A PARTICULAR PURPOSE, ACCURACY, COMPLETENESS, OR 		*/
+/*    NONINFRINGEMENT OF INTELLECTUAL PROPERTY RIGHTS, OR ANY WARRANTY 		*/
+/*    OTHERWISE ARISING OUT OF ANY PROPOSAL, SPECIFICATION OR SAMPLE.		*/
+/*										*/
+/*  - Without limitation, TCG and its members and licensors disclaim all 	*/
+/*    liability, including liability for infringement of any proprietary 	*/
+/*    rights, relating to use of information in this specification and to the	*/
+/*    implementation of this specification, and TCG disclaims all liability for	*/
+/*    cost of procurement of substitute goods or services, lost profits, loss 	*/
+/*    of use, loss of data or any incidental, consequential, direct, indirect, 	*/
+/*    or special damages, whether under contract, tort, warranty or otherwise, 	*/
+/*    arising in any way out of use or reliance upon this specification or any 	*/
+/*    information herein.							*/
+/*										*/
+/*  (c) Copyright IBM Corp. and others, 2023				  	*/
+/*										*/
+/********************************************************************************/
+
+// this file defines the desired command list that should be built into the
+// Tpm Core Lib.
+
+#ifndef _TPM_PROFILE_COMMAND_LIST_H_
+#define _TPM_PROFILE_COMMAND_LIST_H_
+
+#if(YES != 1 || NO != 0)
+#  error YES and NO must be correctly set before including TpmProfile_CommandList.h
+#endif
+#if defined(CC_YES) || defined(CC_NO)
+#  error CC_YES and CC_NO should be defined by the command line file, not before
+#endif
+
+#define CC_YES YES
+#define CC_NO  NO
+
+//
+// Defines for Implemented Commands
+//
+
+// Commands that are defined in the spec, but not implemented for various
+// reasons:
+
+// The TPM reference implementation does not implement attached-component
+// features, and the Compliance test suite has no test cases.
+#define CC_AC_GetCapability CC_NO
+#define CC_AC_Send          CC_NO
+
+// The TPM reference implementation does not implement firmware upgrade.
+#define CC_FieldUpgradeData  CC_NO
+#define CC_FieldUpgradeStart CC_NO
+#define CC_FirmwareRead      CC_NO
+
+// A prototype of CertifyX509 is provided here for informative purposes only.
+// While all of the TPM reference implementation is provided "AS IS" without any
+// warranty, the current design and implementation of CertifyX509 are considered
+// to be especially unsuitable for product use.
+#define CC_CertifyX509 CC_YES
+
+// Normal commands:
+
+#define CC_ACT_SetTimeout             CC_NO		/* libtpms: NO */
+#define CC_ActivateCredential         CC_YES
+#define CC_Certify                    CC_YES
+#define CC_CertifyCreation            CC_YES
+#define CC_ChangeEPS                  CC_YES
+#define CC_ChangePPS                  CC_YES
+#define CC_Clear                      CC_YES
+#define CC_ClearControl               CC_YES
+#define CC_ClockRateAdjust            CC_YES
+#define CC_ClockSet                   CC_YES
+#define CC_Commit                     (CC_YES && ALG_ECC)
+#define CC_ContextLoad                CC_YES
+#define CC_ContextSave                CC_YES
+#define CC_Create                     CC_YES
+#define CC_CreateLoaded               CC_YES
+#define CC_CreatePrimary              CC_YES
+#define CC_DictionaryAttackLockReset  CC_YES
+#define CC_DictionaryAttackParameters CC_YES
+#define CC_Duplicate                  CC_YES
+#define CC_ECC_Decrypt                (CC_NO)		/* libtpms: NO */
+#define CC_ECC_Encrypt                (CC_NO)		/* libtpms: NO */
+#define CC_ECC_Parameters             (CC_YES && ALG_ECC)
+#define CC_ECDH_KeyGen                (CC_YES && ALG_ECC)
+#define CC_ECDH_ZGen                  (CC_YES && ALG_ECC)
+#define CC_EC_Ephemeral               (CC_YES && ALG_ECC)
+#define CC_EncryptDecrypt             CC_YES
+#define CC_EncryptDecrypt2            CC_YES
+#define CC_EventSequenceComplete      CC_YES
+#define CC_EvictControl               CC_YES
+#define CC_FlushContext               CC_YES
+#define CC_GetCapability              CC_YES
+#define CC_GetCommandAuditDigest      CC_YES
+#define CC_GetRandom                  CC_YES
+#define CC_GetSessionAuditDigest      CC_YES
+#define CC_GetTestResult              CC_YES
+#define CC_GetTime                    CC_YES
+#define CC_HMAC                       (CC_YES && !ALG_CMAC)
+#define CC_HMAC_Start                 (CC_YES && !ALG_CMAC)
+#define CC_Hash                       CC_YES
+#define CC_HashSequenceStart          CC_YES
+#define CC_HierarchyChangeAuth        CC_YES
+#define CC_HierarchyControl           CC_YES
+#define CC_Import                     CC_YES
+#define CC_IncrementalSelfTest        CC_YES
+#define CC_Load                       CC_YES
+#define CC_LoadExternal               CC_YES
+#define CC_MAC                        (CC_YES && ALG_CMAC)
+#define CC_MAC_Start                  (CC_YES && ALG_CMAC)
+#define CC_MakeCredential             CC_YES
+#define CC_NV_Certify                 CC_YES
+#define CC_NV_ChangeAuth              CC_YES
+#define CC_NV_DefineSpace             CC_YES
+#define CC_NV_Extend                  CC_YES
+#define CC_NV_GlobalWriteLock         CC_YES
+#define CC_NV_Increment               CC_YES
+#define CC_NV_Read                    CC_YES
+#define CC_NV_ReadLock                CC_YES
+#define CC_NV_ReadPublic              CC_YES
+#define CC_NV_SetBits                 CC_YES
+#define CC_NV_UndefineSpace           CC_YES
+#define CC_NV_UndefineSpaceSpecial    CC_YES
+#define CC_NV_Write                   CC_YES
+#define CC_NV_WriteLock               CC_YES
+#define CC_ObjectChangeAuth           CC_YES
+#define CC_PCR_Allocate               CC_YES
+#define CC_PCR_Event                  CC_YES
+#define CC_PCR_Extend                 CC_YES
+#define CC_PCR_Read                   CC_YES
+#define CC_PCR_Reset                  CC_YES
+#define CC_PCR_SetAuthPolicy          CC_YES
+#define CC_PCR_SetAuthValue           CC_YES
+#define CC_PP_Commands                CC_YES
+#define CC_PolicyAuthValue            CC_YES
+#define CC_PolicyAuthorize            CC_YES
+#define CC_PolicyAuthorizeNV          CC_YES
+#define CC_PolicyCapability           CC_NO	/* libtpms: NO */
+#define CC_PolicyCommandCode          CC_YES
+#define CC_PolicyCounterTimer         CC_YES
+#define CC_PolicyCpHash               CC_YES
+#define CC_PolicyDuplicationSelect    CC_YES
+#define CC_PolicyGetDigest            CC_YES
+#define CC_PolicyLocality             CC_YES
+#define CC_PolicyNV                   CC_YES
+#define CC_PolicyNameHash             CC_YES
+#define CC_PolicyNvWritten            CC_YES
+#define CC_PolicyOR                   CC_YES
+#define CC_PolicyPCR                  CC_YES
+#define CC_PolicyPassword             CC_YES
+#define CC_PolicyParameters           CC_NO 	/* libtpms: NO */
+#define CC_PolicyPhysicalPresence     CC_YES
+#define CC_PolicyRestart              CC_YES
+#define CC_PolicySecret               CC_YES
+#define CC_PolicySigned               CC_YES
+#define CC_PolicyTemplate             CC_YES
+#define CC_PolicyTicket               CC_YES
+#define CC_Policy_AC_SendSelect       CC_NO	/* kgold *//* libtpms: NO */
+#define CC_Quote                      CC_YES
+#define CC_RSA_Decrypt                (CC_YES && ALG_RSA)
+#define CC_RSA_Encrypt                (CC_YES && ALG_RSA)
+#define CC_ReadClock                  CC_YES
+#define CC_ReadPublic                 CC_YES
+#define CC_Rewrap                     CC_YES
+#define CC_SelfTest                   CC_YES
+#define CC_SequenceComplete           CC_YES
+#define CC_SequenceUpdate             CC_YES
+#define CC_SetAlgorithmSet            CC_YES
+#define CC_SetCommandCodeAuditStatus  CC_YES
+#define CC_SetPrimaryPolicy           CC_YES
+#define CC_Shutdown                   CC_YES
+#define CC_Sign                       CC_YES
+#define CC_StartAuthSession           CC_YES
+#define CC_Startup                    CC_YES
+#define CC_StirRandom                 CC_YES
+#define CC_TestParms                  CC_YES
+#define CC_Unseal                     CC_YES
+#define CC_Vendor_TCG_Test            CC_NO	/* libtpms: NO */
+#define CC_VerifySignature            CC_YES
+#define CC_ZGen_2Phase                (CC_YES && ALG_ECC)
+#define CC_NV_DefineSpace2            CC_NO	/* libtpms: NO */
+#define CC_NV_ReadPublic2             CC_NO	/* libtpms: NO */
+#define CC_SetCapability              CC_NO	/* libtpms: NO */
+
+/* kgold */
+#if 0						// libtpms: added
+#define CC_NTC2_PreConfig             CC_YES
+#define CC_NTC2_LockPreConfig         CC_YES
+#define CC_NTC2_GetConfig             CC_YES
+#endif						// libtpms: added
+
+#endif  // _TPM_PROFILE_COMMAND_LIST_H_
---
+++ src/tpm2/TpmProfile_Common.h
@@ -0,0 +1,334 @@
+/********************************************************************************/
+/*										*/
+/*						*/
+/*			     Written by Ken Goldman				*/
+/*		       IBM Thomas J. Watson Research Center			*/
+/*										*/
+/*  Licenses and Notices							*/
+/*										*/
+/*  1. Copyright Licenses:							*/
+/*										*/
+/*  - Trusted Computing Group (TCG) grants to the user of the source code in	*/
+/*    this specification (the "Source Code") a worldwide, irrevocable, 		*/
+/*    nonexclusive, royalty free, copyright license to reproduce, create 	*/
+/*    derivative works, distribute, display and perform the Source Code and	*/
+/*    derivative works thereof, and to grant others the rights granted herein.	*/
+/*										*/
+/*  - The TCG grants to the user of the other parts of the specification 	*/
+/*    (other than the Source Code) the rights to reproduce, distribute, 	*/
+/*    display, and perform the specification solely for the purpose of 		*/
+/*    developing products based on such documents.				*/
+/*										*/
+/*  2. Source Code Distribution Conditions:					*/
+/*										*/
+/*  - Redistributions of Source Code must retain the above copyright licenses, 	*/
+/*    this list of conditions and the following disclaimers.			*/
+/*										*/
+/*  - Redistributions in binary form must reproduce the above copyright 	*/
+/*    licenses, this list of conditions	and the following disclaimers in the 	*/
+/*    documentation and/or other materials provided with the distribution.	*/
+/*										*/
+/*  3. Disclaimers:								*/
+/*										*/
+/*  - THE COPYRIGHT LICENSES SET FORTH ABOVE DO NOT REPRESENT ANY FORM OF	*/
+/*  LICENSE OR WAIVER, EXPRESS OR IMPLIED, BY ESTOPPEL OR OTHERWISE, WITH	*/
+/*  RESPECT TO PATENT RIGHTS HELD BY TCG MEMBERS (OR OTHER THIRD PARTIES)	*/
+/*  THAT MAY BE NECESSARY TO IMPLEMENT THIS SPECIFICATION OR OTHERWISE.		*/
+/*  Contact TCG Administration (admin@trustedcomputinggroup.org) for 		*/
+/*  information on specification licensing rights available through TCG 	*/
+/*  membership agreements.							*/
+/*										*/
+/*  - THIS SPECIFICATION IS PROVIDED "AS IS" WITH NO EXPRESS OR IMPLIED 	*/
+/*    WARRANTIES WHATSOEVER, INCLUDING ANY WARRANTY OF MERCHANTABILITY OR 	*/
+/*    FITNESS FOR A PARTICULAR PURPOSE, ACCURACY, COMPLETENESS, OR 		*/
+/*    NONINFRINGEMENT OF INTELLECTUAL PROPERTY RIGHTS, OR ANY WARRANTY 		*/
+/*    OTHERWISE ARISING OUT OF ANY PROPOSAL, SPECIFICATION OR SAMPLE.		*/
+/*										*/
+/*  - Without limitation, TCG and its members and licensors disclaim all 	*/
+/*    liability, including liability for infringement of any proprietary 	*/
+/*    rights, relating to use of information in this specification and to the	*/
+/*    implementation of this specification, and TCG disclaims all liability for	*/
+/*    cost of procurement of substitute goods or services, lost profits, loss 	*/
+/*    of use, loss of data or any incidental, consequential, direct, indirect, 	*/
+/*    or special damages, whether under contract, tort, warranty or otherwise, 	*/
+/*    arising in any way out of use or reliance upon this specification or any 	*/
+/*    information herein.							*/
+/*										*/
+/*  (c) Copyright IBM Corp. and others, 2023				  	*/
+/*										*/
+/********************************************************************************/
+
+// clang-format off
+// clang-format off to preserve define alignment breaking sections.
+
+// this file defines the common optional selections for the TPM library build
+// Requires basic YES/NO defines are already set (by TpmBuildSwitches.h)
+// Less frequently changed items are in other TpmProfile Headers.
+
+#ifndef _TPM_PROFILE_COMMON_H_
+#define _TPM_PROFILE_COMMON_H_
+// YES & NO defined by TpmBuildSwitches.h
+#if (YES != 1 || NO != 0)
+#  error YES or NO incorrectly set
+#endif
+#if defined(ALG_YES) || defined(ALG_NO)
+#  error ALG_YES and ALG_NO should only be defined by the TpmProfile_Common.h file
+#endif
+
+// Change these definitions to turn all algorithms ON or OFF. That is, to turn
+// all algorithms on, set ALG_NO to YES. This is intended as a debug feature.
+#define  ALG_YES                    YES
+#define  ALG_NO                     NO
+
+// Defines according to the processor being built for.
+// Are building for a BIG_ENDIAN processor?
+#if defined __FreeBSD__ || defined __DragonFly__   /* libtpms added begin */
+# include <sys/endian.h>
+#elif defined __APPLE__
+# include <libkern/OSByteOrder.h>
+#else
+# include <endian.h>
+#endif
+#if defined __linux__ || defined __CYGWIN__
+ #if __BYTE_ORDER == __LITTLE_ENDIAN
+  #define  BIG_ENDIAN_TPM       NO
+ #endif
+ #if __BYTE_ORDER == __BIG_ENDIAN
+  #define  BIG_ENDIAN_TPM       YES
+ #endif
+#elif defined __OpenBSD__ || defined __FreeBSD__ || defined __NetBSD__ \
+   || defined __DragonFly__
+ #if _BYTE_ORDER == _LITTLE_ENDIAN
+  #define  BIG_ENDIAN_TPM       NO
+ #endif
+ #if _BYTE_ORDER == _BIG_ENDIAN
+  #define  BIG_ENDIAN_TPM       YES
+ #endif
+#elif defined __APPLE__
+ #define  BIG_ENDIAN_TPM       NO
+#else
+ #error Unsupported OS
+#endif                                             /* libtpms added end */
+#ifndef BIG_ENDIAN_TPM
+#define  BIG_ENDIAN_TPM             NO
+#endif
+#define  LITTLE_ENDIAN_TPM          !BIG_ENDIAN_TPM
+// Does the processor put the most-significant bit at bit position 0?
+#define MOST_SIGNIFICANT_BIT_0      NO
+#define LEAST_SIGNIFICANT_BIT_0     !MOST_SIGNIFICANT_BIT_0
+// Does processor support Auto align?
+#define  AUTO_ALIGN                 NO
+
+//***********************************************
+// Defines for Symmetric Algorithms
+//***********************************************
+
+#define ALG_AES                     ALG_YES
+
+#define     AES_128                     (YES * ALG_AES)
+#define     AES_192                     (NO  * ALG_AES)
+#define     AES_256                     (YES * ALG_AES)
+
+#define ALG_SM4                     ALG_NO
+
+#define     SM4_128                     (NO  * ALG_SM4)
+
+#define ALG_CAMELLIA                ALG_YES
+
+#define     CAMELLIA_128                (YES * ALG_CAMELLIA)
+#define     CAMELLIA_192                (NO  * ALG_CAMELLIA)
+#define     CAMELLIA_256                (YES * ALG_CAMELLIA)
+
+#define ALG_TDES                        ALG_YES /* libtpms enabled */
+
+#define     TDES_128                    (ALG_TDES && YES)
+#define     TDES_192                    (ALG_TDES && YES)
+
+// must be yes if any above are yes.
+#define ALG_SYMCIPHER               (ALG_AES || ALG_SM4 || ALG_CAMELLIA)
+#define ALG_CMAC                    (YES * ALG_SYMCIPHER)
+
+// block cipher modes
+#define ALG_CTR                     ALG_YES
+#define ALG_OFB                     ALG_YES
+#define ALG_CBC                     ALG_YES
+#define ALG_CFB                     ALG_YES
+#define ALG_ECB                     ALG_YES
+
+//***********************************************
+// Defines for RSA Asymmetric Algorithms
+//***********************************************
+#define ALG_RSA                     ALG_YES
+#define     RSA_1024                        (YES * ALG_RSA)
+#define     RSA_2048                        (YES * ALG_RSA)
+#define     RSA_3072                        (YES * ALG_RSA)
+#define     RSA_4096                        (NO  * ALG_RSA)	/* libtpms: NO */
+#define     RSA_16384                       (NO  * ALG_RSA)
+
+#define     ALG_RSASSA                      (YES * ALG_RSA)
+#define     ALG_RSAES                       (YES * ALG_RSA)
+#define     ALG_RSAPSS                      (YES * ALG_RSA)
+#define     ALG_OAEP                        (YES * ALG_RSA)
+
+// RSA Implementation Styles
+// use Chinese Remainder Theorem (5 prime) format for private key ?
+#define CRT_FORMAT_RSA                  YES
+#define RSA_DEFAULT_PUBLIC_EXPONENT     0x00010001
+
+//***********************************************
+// Defines for ECC Asymmetric Algorithms
+//***********************************************
+#define ALG_ECC                     ALG_YES
+#define     ALG_ECDH                        (YES * ALG_ECC)
+#define     ALG_ECDSA                       (YES * ALG_ECC)
+#define     ALG_ECDAA                       (YES * ALG_ECC)
+#define     ALG_SM2                         (YES * ALG_ECC)
+#define     ALG_ECSCHNORR                   (YES * ALG_ECC)
+#define     ALG_ECMQV                       (YES * ALG_ECC)
+#define     ALG_KDF1_SP800_56A              (YES * ALG_ECC)
+#define     ALG_EDDSA                       (NO  * ALG_ECC)
+#define     ALG_EDDSA_PH                    (NO  * ALG_ECC)
+
+#define     ECC_NIST_P192                   (YES * ALG_ECC)
+#define     ECC_NIST_P224                   (YES * ALG_ECC)
+#define     ECC_NIST_P256                   (YES * ALG_ECC)
+#define     ECC_NIST_P384                   (YES * ALG_ECC)
+#define     ECC_NIST_P521                   (YES * ALG_ECC)
+#define     ECC_BN_P256                     (YES * ALG_ECC)
+#define     ECC_BN_P638                     (YES * ALG_ECC)
+#define     ECC_SM2_P256                    (YES * ALG_ECC)
+
+#define     ECC_BP_P256_R1                  (NO * ALG_ECC)
+#define     ECC_BP_P384_R1                  (NO * ALG_ECC)
+#define     ECC_BP_P512_R1                  (NO * ALG_ECC)
+#define     ECC_CURVE_25519                 (NO * ALG_ECC)
+#define     ECC_CURVE_448                   (NO * ALG_ECC)
+
+//***********************************************
+// Defines for Hash/XOF Algorithms
+//***********************************************
+#define ALG_MGF1                            ALG_YES
+#define ALG_SHA1                            ALG_YES
+#define ALG_SHA256                          ALG_YES
+#define ALG_SHA256_192                      ALG_NO
+#define ALG_SHA384                          ALG_YES
+#define ALG_SHA512                          ALG_YES
+
+#define ALG_SHA3_256                        ALG_NO
+#define ALG_SHA3_384                        ALG_NO
+#define ALG_SHA3_512                        ALG_NO
+
+#define ALG_SM3_256                         ALG_NO
+
+#define ALG_SHAKE256_192                    ALG_NO
+#define ALG_SHAKE256_256                    ALG_NO
+#define ALG_SHAKE256_512                    ALG_NO
+
+//***********************************************
+// Defines for Stateful Signature Algorithms
+//***********************************************
+#define ALG_LMS                             ALG_NO
+#define ALG_XMSS                            ALG_NO
+
+//***********************************************
+// Defines for Keyed Hashes
+//***********************************************
+#define ALG_KEYEDHASH                       ALG_YES
+#define ALG_HMAC                            ALG_YES
+
+//***********************************************
+// Defines for KDFs
+//***********************************************
+#define ALG_KDF2                            ALG_YES
+#define ALG_KDF1_SP800_108                  ALG_YES
+
+//***********************************************
+// Defines for Obscuration/MISC/compatibility
+//***********************************************
+#define ALG_XOR                             ALG_YES
+
+//***********************************************
+// Defines controlling ACT
+//***********************************************
+#define ACT_SUPPORT                         YES
+#define RH_ACT_0                                ( NO * ACT_SUPPORT)	/* libtpms: NO */
+#define RH_ACT_1                                ( NO * ACT_SUPPORT)
+#define RH_ACT_2                                ( NO * ACT_SUPPORT)
+#define RH_ACT_3                                ( NO * ACT_SUPPORT)
+#define RH_ACT_4                                ( NO * ACT_SUPPORT)
+#define RH_ACT_5                                ( NO * ACT_SUPPORT)
+#define RH_ACT_6                                ( NO * ACT_SUPPORT)
+#define RH_ACT_7                                ( NO * ACT_SUPPORT)
+#define RH_ACT_8                                ( NO * ACT_SUPPORT)
+#define RH_ACT_9                                ( NO * ACT_SUPPORT)
+#define RH_ACT_A                                ( NO * ACT_SUPPORT)	 /* libtpms: NO */
+#define RH_ACT_B                                ( NO * ACT_SUPPORT)
+#define RH_ACT_C                                ( NO * ACT_SUPPORT)
+#define RH_ACT_D                                ( NO * ACT_SUPPORT)
+#define RH_ACT_E                                ( NO * ACT_SUPPORT)
+#define RH_ACT_F                                ( NO * ACT_SUPPORT)
+// libtpms added begin
+#if RH_ACT_0 + RH_ACT_1 + RH_ACT_2 + RH_ACT_3 + RH_ACT_4 + \
+    RH_ACT_5 + RH_ACT_6 + RH_ACT_7 + RH_ACT_8 + RH_ACT_9 + \
+    RH_ACT_A + RH_ACT_B + RH_ACT_C + RH_ACT_D + RH_ACT_E + \
+    RH_ACT_F == 0
+#define __ACT_DISABLED
+#endif
+// libtpms added end
+
+
+#if 0							// libtpms: added
+//***********************************************
+// Enable VENDOR_PERMANENT_AUTH_HANDLE?
+//***********************************************
+#define VENDOR_PERMANENT_AUTH_ENABLED       NO
+// if YES, this must be valid per Part2 (TPM_RH_AUTH_00 - TPM_RH_AUTH_FF)
+// if NO, this must be #undef
+#undef  VENDOR_PERMANENT_AUTH_HANDLE
+#endif							// libtpms: added
+
+//***********************************************
+// Defines controlling optional implementation
+//***********************************************
+#define FIELD_UPGRADE_IMPLEMENTED           NO
+
+//***********************************************
+// Buffer Sizes based on implementation
+//***********************************************
+// When using PC CRB, the page size for both commands and
+// control registers is 4k.  The command buffer starts at
+// offset 0x80, so the net size available is:
+#define  MAX_COMMAND_SIZE               TPM2_GetBufferSize()	/* libtpms: changed */
+#define  MAX_RESPONSE_SIZE              TPM2_GetBufferSize()	/* libtpms: changed */
+
+//***********************************************
+// Vendor Info
+//***********************************************
+// max buffer for vendor commands
+// Max data buffer leaving space for TPM2B size prefix
+#define VENDOR_COMMAND_COUNT          0
+#define MAX_VENDOR_BUFFER_SIZE        1024 /* libtpms: changed from (MAX_RESPONSE_SIZE-2) */
+#define PRIVATE_VENDOR_SPECIFIC_BYTES \
+    ((MAX_RSA_KEY_BYTES/2) * (3 + CRT_FORMAT_RSA * 2))  /* libtpms: keep as was in rev 150 */
+
+//***********************************************
+// Defines controlling Firmware- and SVN-limited objects
+//***********************************************
+#if 0							// libtpms: added
+#define FW_LIMITED_SUPPORT                    YES
+#define SVN_LIMITED_SUPPORT                   YES
+#endif							// libtpms: added
+
+//***********************************************
+// Defines controlling External NV
+//***********************************************
+// This is a software reference implementation of the TPM: there is no
+// "external NV" as such. This #define configures the TPM to implement
+// "external NV" that is stored in the same place as "internal NV."
+// NOTE: enabling this doesn't necessarily mean that the expanded
+// (external-NV-specific) attributes are supported.
+#if 0							// libtpms: added
+#define EXTERNAL_NV                           YES
+#endif							// libtpms: added
+
+#endif // _TPM_PROFILE_COMMON_H_
