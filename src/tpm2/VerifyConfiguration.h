aa7219cae792b1e1919af121eee1c5d0d1848de9
Stefan Berger <stefanb@linux.ibm.com>
2023-12-14 12:49:17 -0500

    rev180: Add tpm_public.h and VerifyConfiguration.h
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- src/tpm2/VerifyConfiguration.h
+++ src/tpm2/VerifyConfiguration.h
@@ -1,6 +1,6 @@
 /********************************************************************************/
 /*										*/
-/*		Root header file for building any TPM.lib code			*/
+/*						*/
 /*			     Written by Ken Goldman				*/
 /*		       IBM Thomas J. Watson Research Center			*/
 /*										*/
@@ -54,24 +54,99 @@
 /*    arising in any way out of use or reliance upon this specification or any 	*/
 /*    information herein.							*/
 /*										*/
-/*  (c) Copyright IBM Corp. and others, 2016 - 2023				*/
+/*  (c) Copyright IBM Corp. and others, 2023				  	*/
 /*										*/
 /********************************************************************************/
 
-// Root header file for building any TPM.lib code
+//
+// This verifies that information expected from the consumer's TpmConfiguration is
+// set properly and consistently.
+//
+#ifndef _VERIFY_CONFIGURATION_H
+#define _VERIFY_CONFIGURATION_H
 
-#ifndef _TPM_H_
-#define _TPM_H_
-// TODO_RENAME_INC_FOLDER: public refers to the TPM_CoreLib public headers
-#include "tpm_public.h"
+// verify these defines are either YES or NO.
+#define MUST_BE_0_OR_1(x) MUST_BE(((x) == 0) || ((x) == 1))
 
-#include "TpmAlgorithmDefines.h"
-#include "LibSupport.h"        // Types from the library. These need to come before
-// Global.h because some of the structures in
-// that file depend on the structures used by the
-// cryptographic libraries.
-#include "GpMacros.h"          // Define additional macros
-#include "Global.h"            // Define other TPM types
-#include "InternalRoutines.h"  // Function prototypes
+// Debug Options
+MUST_BE_0_OR_1(DEBUG);
+MUST_BE_0_OR_1(SIMULATION);
+MUST_BE_0_OR_1(DRBG_DEBUG_PRINT);
+MUST_BE_0_OR_1(CERTIFYX509_DEBUG);
+MUST_BE_0_OR_1(USE_DEBUG_RNG);
 
-#endif  // _TPM_H_
+// RSA Debug Options
+MUST_BE_0_OR_1(RSA_INSTRUMENT);
+MUST_BE_0_OR_1(USE_RSA_KEY_CACHE);
+MUST_BE_0_OR_1(USE_KEY_CACHE_FILE);
+
+// Test Options
+MUST_BE_0_OR_1(ALLOW_FORCE_FAILURE_MODE);
+
+// Internal checks
+MUST_BE_0_OR_1(LIBRARY_COMPATIBILITY_CHECK);
+MUST_BE_0_OR_1(COMPILER_CHECKS);
+MUST_BE_0_OR_1(RUNTIME_SIZE_CHECKS);
+
+// Compliance options
+MUST_BE_0_OR_1(FIPS_COMPLIANT);
+MUST_BE_0_OR_1(USE_SPEC_COMPLIANT_PROOFS);
+MUST_BE_0_OR_1(SKIP_PROOF_ERRORS);
+
+// Implementation alternatives - should not change external behavior
+MUST_BE_0_OR_1(TABLE_DRIVEN_DISPATCH);
+MUST_BE_0_OR_1(TABLE_DRIVEN_MARSHAL);
+MUST_BE_0_OR_1(USE_MARSHALING_DEFINES);
+MUST_BE_0_OR_1(COMPRESSED_LISTS);
+MUST_BE_0_OR_1(USE_BIT_FIELD_STRUCTURES);
+MUST_BE_0_OR_1(RSA_KEY_SIEVE);
+
+// Implementation alternatives - changes external behavior
+MUST_BE_0_OR_1(_DRBG_STATE_SAVE);
+MUST_BE_0_OR_1(USE_DA_USED);
+MUST_BE_0_OR_1(SELF_TEST);
+MUST_BE_0_OR_1(CLOCK_STOPS);
+MUST_BE_0_OR_1(ACCUMULATE_SELF_HEAL_TIMER);
+MUST_BE_0_OR_1(FAIL_TRACE);
+
+// Vendor alternatives
+// Check VENDOR_PERMANENT_AUTH_ENABLED & VENDOR_PERMANENT_AUTH_HANDLE are consistent
+MUST_BE_0_OR_1(VENDOR_PERMANENT_AUTH_ENABLED);
+
+#if VENDOR_PERMANENT_AUTH_ENABLED == YES
+#  if !defined(VENDOR_PERMANENT_AUTH_HANDLE)		       \
+    || VENDOR_PERMANENT_AUTH_HANDLE < TPM_RH_AUTH_00			\
+    || VENDOR_PERMANENT_AUTH_HANDLE > TPM_RH_AUTH_FF
+#    error VENDOR_PERMANENT_AUTH_ENABLED requires a valid definition for VENDOR_PERMANENT_AUTH_HANDLE, see Part2
+#  endif
+#else
+#  if defined(VENDOR_PERMANENT_AUTH_HANDLE)
+#    error VENDOR_PERMANENT_AUTH_HANDLE requires VENDOR_PERMANENT_AUTH_ENABLED to be YES
+#  endif
+#endif
+
+// now check for inconsistent combinations of options
+#if USE_KEY_CACHE_FILE && !USE_RSA_KEY_CACHE
+#  error cannot use USE_KEY_CACHE_FILE if not using USE_RSA_KEY_CACHE
+#endif
+
+#if !DEBUG
+#  if USE_KEY_CACHE_FILE || USE_RSA_KEY_CACHE || DRBG_DEBUG_PRINT	\
+    || CERTIFYX509_DEBUG || USE_DEBUG_RNG
+#    error using insecure options not in DEBUG mode.
+#  endif
+#endif
+
+#if !SIMULATION
+#  if USE_KEY_CACHE_FILE
+#    error USE_KEY_CACHE_FILE requires SIMULATION
+#  endif
+#  if RSA_INSTRUMENT
+#    error RSA_INSTRUMENT requires SIMULATION
+#  endif
+#  if USE_DEBUG_RNG
+#    error USE_DEBUG_RNG requires SIMULATION
+#  endif
+#endif
+
+#endif  // _VERIFY_CONFIGURATION_H
