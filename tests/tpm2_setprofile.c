0946a5cf7cd5af0b8918f01527bec4b6ef51df13
Stefan Berger <stefanb@linux.ibm.com>
2024-08-23 11:06:36 -0400

    tpm2: Allow setting the minimum HMAC key size: hmac-min-key-size
    Allow setting the minimum HMAC key size and add enforcement gates.
    
    Check that the value of hmac=min-key-size given in the profile is not
    larger than 1024. This value is taken from the maximum size of
    TPM2B_SENSITIVE, which is MAX_SYM_DATA (=128), which can be provided as
    key to an HMAC.
    
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- tests/tpm2_setprofile.c
+++ tests/tpm2_setprofile.c
@@ -352,6 +352,37 @@ static const struct {
             "\"Description\":\"test\""
           "}}",
     }, {
+        .profile = "{" /* StateFormatLevel 7 is chosen */
+                    "\"Name\":\"custom:test\","
+                    "\"Algorithms\":\"rsa,rsa-min-size=1024,tdes,tdes-min-size=128,"
+                                     "sha1,hmac,aes,aes-min-size=128,mgf1,keyedhash,"
+                                     "xor,sha256,sha384,sha512,null,rsassa,rsaes,rsapss,"
+                                     "oaep,ecdsa,ecdh,ecdaa,sm2,ecschnorr,ecmqv,"
+                                     "kdf1-sp800-56a,kdf2,kdf1-sp800-108,ecc,ecc-min-size=192,"
+                                     "ecc-nist,ecc-bn,ecc-sm2-p256,symcipher,camellia,"
+                                     "camellia-min-size=128,cmac,ctr,ofb,cbc,cfb,ecb,"
+                                     "hmac-min-key-size=128\","
+                    "\"Description\":\"test\""
+                   "}",
+        .exp_fail = false,
+        .exp_profile =
+          "{\"ActiveProfile\":{"
+            "\"Name\":\"custom:test\","
+            "\"StateFormatLevel\":7,"
+            "\"Commands\":\"0x11f-0x122,0x124-0x12e,0x130-0x140,0x142-0x159,"
+                           "0x15b-0x15e,0x160-0x165,0x167-0x174,0x176-0x178,"
+                           "0x17a-0x193,0x197,0x199-0x19c\","
+            "\"Algorithms\":\"rsa,rsa-min-size=1024,tdes,tdes-min-size=128,"
+                             "sha1,hmac,aes,aes-min-size=128,mgf1,keyedhash,"
+                             "xor,sha256,sha384,sha512,null,rsassa,rsaes,rsapss,"
+                             "oaep,ecdsa,ecdh,ecdaa,sm2,ecschnorr,ecmqv,"
+                             "kdf1-sp800-56a,kdf2,kdf1-sp800-108,ecc,ecc-min-size=192,"
+                             "ecc-nist,ecc-bn,ecc-sm2-p256,symcipher,camellia,"
+                             "camellia-min-size=128,cmac,ctr,ofb,cbc,cfb,ecb,"
+                             "hmac-min-key-size=128\","
+            "\"Description\":\"test\""
+          "}}",
+    }, {
         // keep last
     }
 };
