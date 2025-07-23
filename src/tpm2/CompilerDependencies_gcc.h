84e1aaa64af58e4b3a660c7adff5988320a98ece
Stefan Berger <stefanb@linux.ibm.com>
2025-07-23 09:50:42 -0400

    tpm2: Include stdint.h/def.h in BaseTypes.h and some other changes
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- src/tpm2/CompilerDependencies_gcc.h
+++ src/tpm2/CompilerDependencies_gcc.h
@@ -1,62 +1,4 @@
-/********************************************************************************/
-/*										*/
-/*						*/
-/*			     Written by Ken Goldman				*/
-/*		       IBM Thomas J. Watson Research Center			*/
-/*										*/
-/*  Licenses and Notices							*/
-/*										*/
-/*  1. Copyright Licenses:							*/
-/*										*/
-/*  - Trusted Computing Group (TCG) grants to the user of the source code in	*/
-/*    this specification (the "Source Code") a worldwide, irrevocable, 		*/
-/*    nonexclusive, royalty free, copyright license to reproduce, create 	*/
-/*    derivative works, distribute, display and perform the Source Code and	*/
-/*    derivative works thereof, and to grant others the rights granted herein.	*/
-/*										*/
-/*  - The TCG grants to the user of the other parts of the specification 	*/
-/*    (other than the Source Code) the rights to reproduce, distribute, 	*/
-/*    display, and perform the specification solely for the purpose of 		*/
-/*    developing products based on such documents.				*/
-/*										*/
-/*  2. Source Code Distribution Conditions:					*/
-/*										*/
-/*  - Redistributions of Source Code must retain the above copyright licenses, 	*/
-/*    this list of conditions and the following disclaimers.			*/
-/*										*/
-/*  - Redistributions in binary form must reproduce the above copyright 	*/
-/*    licenses, this list of conditions	and the following disclaimers in the 	*/
-/*    documentation and/or other materials provided with the distribution.	*/
-/*										*/
-/*  3. Disclaimers:								*/
-/*										*/
-/*  - THE COPYRIGHT LICENSES SET FORTH ABOVE DO NOT REPRESENT ANY FORM OF	*/
-/*  LICENSE OR WAIVER, EXPRESS OR IMPLIED, BY ESTOPPEL OR OTHERWISE, WITH	*/
-/*  RESPECT TO PATENT RIGHTS HELD BY TCG MEMBERS (OR OTHER THIRD PARTIES)	*/
-/*  THAT MAY BE NECESSARY TO IMPLEMENT THIS SPECIFICATION OR OTHERWISE.		*/
-/*  Contact TCG Administration (admin@trustedcomputinggroup.org) for 		*/
-/*  information on specification licensing rights available through TCG 	*/
-/*  membership agreements.							*/
-/*										*/
-/*  - THIS SPECIFICATION IS PROVIDED "AS IS" WITH NO EXPRESS OR IMPLIED 	*/
-/*    WARRANTIES WHATSOEVER, INCLUDING ANY WARRANTY OF MERCHANTABILITY OR 	*/
-/*    FITNESS FOR A PARTICULAR PURPOSE, ACCURACY, COMPLETENESS, OR 		*/
-/*    NONINFRINGEMENT OF INTELLECTUAL PROPERTY RIGHTS, OR ANY WARRANTY 		*/
-/*    OTHERWISE ARISING OUT OF ANY PROPOSAL, SPECIFICATION OR SAMPLE.		*/
-/*										*/
-/*  - Without limitation, TCG and its members and licensors disclaim all 	*/
-/*    liability, including liability for infringement of any proprietary 	*/
-/*    rights, relating to use of information in this specification and to the	*/
-/*    implementation of this specification, and TCG disclaims all liability for	*/
-/*    cost of procurement of substitute goods or services, lost profits, loss 	*/
-/*    of use, loss of data or any incidental, consequential, direct, indirect, 	*/
-/*    or special damages, whether under contract, tort, warranty or otherwise, 	*/
-/*    arising in any way out of use or reliance upon this specification or any 	*/
-/*    information herein.							*/
-/*										*/
-/*  (c) Copyright IBM Corp. and others, 2023				  	*/
-/*										*/
-/********************************************************************************/
+// SPDX-License-Identifier: BSD-2-Clause
 
 // This file contains compiler specific switches.
 // These definitions are for the GCC compiler
@@ -69,11 +11,17 @@
 #  error CompilerDependencies_gcc.h included for wrong compiler
 #endif
 
+// silence specific GCC errors
+#pragma GCC diagnostic push
 // don't warn on unused local typedefs, they are used as a
 // cross-compiler static_assert
-#pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-local-typedefs"
-#pragma GCC diagnostic pop
+
+// This is needed when compiling against OpenSSL 3.0, as the Ossl bindings use:
+//  - EC_POINT_set_affine_coordinates_GFp / EC_POINTs_mul
+//  - AES_set_encrypt_key / AES_encrypt
+//  - Camellia_set_key / Camellia_encrypt
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 
 #undef _MSC_VER
 #undef WIN32
@@ -118,6 +66,12 @@
 
 #define NORETURN __attribute__((noreturn))
 
-#define TPM_INLINE           inline __attribute__((always_inline))
-#define TPM_STATIC_ASSERT(e) _Static_assert(e, "static assert")
+#define TPM_INLINE inline __attribute__((always_inline))
+
+#ifdef __cplusplus
+// c++ needed for google test
+#  define TPM_STATIC_ASSERT(e) static_assert(e, "static assert")
+#else
+#  define TPM_STATIC_ASSERT(e) _Static_assert(e, "static assert")
+#endif
 #endif  // _COMPILER_DEPENDENCIES_H_
