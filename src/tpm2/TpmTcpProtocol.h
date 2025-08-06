59f99e5b62950ae0d42323fcf5147369a102d4c7
Stefan Berger <stefanb@linux.ibm.com>
2025-08-06 15:07:18 -0400

    Sync: Add options for simulator (unused)
    Signed-off-by: Stefan Berger <stefanb@linux.ibm.com>

--- src/tpm2/TpmTcpProtocol.h
+++ src/tpm2/TpmTcpProtocol.h
@@ -1,63 +1,4 @@
-/********************************************************************************/
-/*										*/
-/*	TPM commands are communicated as BYTE streams on a TCP connection	*/
-/*			     Written by Ken Goldman				*/
-/*		       IBM Thomas J. Watson Research Center			*/
-/*            $Id: TpmTcpProtocol.h 1658 2021-01-22 23:14:01Z kgoldman $	*/
-/*										*/
-/*  Licenses and Notices							*/
-/*										*/
-/*  1. Copyright Licenses:							*/
-/*										*/
-/*  - Trusted Computing Group (TCG) grants to the user of the source code in	*/
-/*    this specification (the "Source Code") a worldwide, irrevocable, 		*/
-/*    nonexclusive, royalty free, copyright license to reproduce, create 	*/
-/*    derivative works, distribute, display and perform the Source Code and	*/
-/*    derivative works thereof, and to grant others the rights granted herein.	*/
-/*										*/
-/*  - The TCG grants to the user of the other parts of the specification 	*/
-/*    (other than the Source Code) the rights to reproduce, distribute, 	*/
-/*    display, and perform the specification solely for the purpose of 		*/
-/*    developing products based on such documents.				*/
-/*										*/
-/*  2. Source Code Distribution Conditions:					*/
-/*										*/
-/*  - Redistributions of Source Code must retain the above copyright licenses, 	*/
-/*    this list of conditions and the following disclaimers.			*/
-/*										*/
-/*  - Redistributions in binary form must reproduce the above copyright 	*/
-/*    licenses, this list of conditions	and the following disclaimers in the 	*/
-/*    documentation and/or other materials provided with the distribution.	*/
-/*										*/
-/*  3. Disclaimers:								*/
-/*										*/
-/*  - THE COPYRIGHT LICENSES SET FORTH ABOVE DO NOT REPRESENT ANY FORM OF	*/
-/*  LICENSE OR WAIVER, EXPRESS OR IMPLIED, BY ESTOPPEL OR OTHERWISE, WITH	*/
-/*  RESPECT TO PATENT RIGHTS HELD BY TCG MEMBERS (OR OTHER THIRD PARTIES)	*/
-/*  THAT MAY BE NECESSARY TO IMPLEMENT THIS SPECIFICATION OR OTHERWISE.		*/
-/*  Contact TCG Administration (admin@trustedcomputinggroup.org) for 		*/
-/*  information on specification licensing rights available through TCG 	*/
-/*  membership agreements.							*/
-/*										*/
-/*  - THIS SPECIFICATION IS PROVIDED "AS IS" WITH NO EXPRESS OR IMPLIED 	*/
-/*    WARRANTIES WHATSOEVER, INCLUDING ANY WARRANTY OF MERCHANTABILITY OR 	*/
-/*    FITNESS FOR A PARTICULAR PURPOSE, ACCURACY, COMPLETENESS, OR 		*/
-/*    NONINFRINGEMENT OF INTELLECTUAL PROPERTY RIGHTS, OR ANY WARRANTY 		*/
-/*    OTHERWISE ARISING OUT OF ANY PROPOSAL, SPECIFICATION OR SAMPLE.		*/
-/*										*/
-/*  - Without limitation, TCG and its members and licensors disclaim all 	*/
-/*    liability, including liability for infringement of any proprietary 	*/
-/*    rights, relating to use of information in this specification and to the	*/
-/*    implementation of this specification, and TCG disclaims all liability for	*/
-/*    cost of procurement of substitute goods or services, lost profits, loss 	*/
-/*    of use, loss of data or any incidental, consequential, direct, indirect, 	*/
-/*    or special damages, whether under contract, tort, warranty or otherwise, 	*/
-/*    arising in any way out of use or reliance upon this specification or any 	*/
-/*    information herein.							*/
-/*										*/
-/*  (c) Copyright IBM Corp. and others, 2016 - 2021				*/
-/*										*/
-/********************************************************************************/
+// SPDX-License-Identifier: BSD-2-Clause
 
 //** Introduction
 
@@ -121,7 +62,25 @@ enum TpmEndPointInfo
     tpmPlatformAvailable = 0x01,
     tpmUsesTbs           = 0x02,
     tpmInRawMode         = 0x04,
-    tpmSupportsPP        = 0x08
+    tpmSupportsPP        = 0x08,
+
+    // Valid only with PlatformAvailable set.
+    // System and TPM power control signals (SignalPowerOn/Off) are not supported.
+    NoPowerCtl = 0x10,
+
+    // Valid only with tpmPlatformAvailable set.
+    // TPM locality cannot be changed.
+    NoLocalityCtl = 0x20,
+
+    // Valid only with tpmPlatformAvailable set.
+    // NV control signals (SignalNvOn/Off) are not supported.
+    NoNvCtl = 0x40,
+
+    // indicates that no force failure mode is available in the protocol
+    NoForceFailure = 0x80,
+
+    // indicates Locality 4 Hash indications are not supported
+    NoHashDataCtl = 0x100
 };
 
 #ifdef _MSC_VER
